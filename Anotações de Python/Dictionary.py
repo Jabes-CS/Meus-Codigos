870023e1fc2c09533fbc5a8c053716ac02d0ab86
Jabes <jabes.candido!@gmail.com>
2024-01-08 12:20:06 -0300

    remoção do arquivo

--- Anotações de Python/Dictionary.py
+++
@@ -1,4 +0,0 @@
-#dictionary
-#Dessa forma temos a chave do lado esquerdo e o valor do lado direito, sendo assim, quando necessário usaremos a chave para recuperar o valor
-instrutores = {'Nico' : 39, 'Flavio': 37, 'Marcos' : 30} #Devemos iniciar dessa forma
-print(instrutores['Flavio']) #Será impresso o valor = 37
