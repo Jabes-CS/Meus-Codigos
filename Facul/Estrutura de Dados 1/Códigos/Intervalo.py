925637d70a9d412df3e6eb1c0e92ea81d2c2b5f1
Jabes <jabes.candido!@gmail.com>
2024-01-08 12:30:22 -0300

    Atualizando/removendo alguns arquivos

--- Facul/Estrutura de Dados 1/Códigos/Intervalo.py
+++
@@ -1,5 +0,0 @@
-def imprime_intervalo():
-    V_inicial = 1
-    V_final = 10
-
-    for i = V_inicial and V_final <= 
--- Facul/Estrutura de Dados 1/Códigos/Vetor.py
+++
@@ -1,5 +0,0 @@
-vetor = [10, 20, 30, 40, 50]
-
-for elemento in vetor:
-    print(elemento)
-    
