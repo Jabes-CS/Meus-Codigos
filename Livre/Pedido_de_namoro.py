fc2e658a5c7bd98f35c34db9ef014afe1316a2da
Your Name <jabes.candido1@gmail.com>
2025-04-02 21:40:40 -0300

    .

--- Livre/Pedido_de_namoro.py
+++ Livre/Pedido_de_namoro.py
@@ -24,18 +24,14 @@ def denied():
     button_1.destroy()
 
 
-margin = Canvas(root, width = 500, bg = '#ffc8dd', height = 100,
-                bd = 0, highlightthickness = 0, relief = 'ridge')
+margin = Canvas(root, width = 500, bg = '#ffc8dd', height = 100, bd = 0, highlightthickness = 0, relief = 'ridge')
 margin.pack()
-text_id = Label(root, bg = '#ffc8dd', text = 'Quer namorar comigo?' ,
-                fg = '#590d22', font = ('Montserrat', 24, 'bold'))
+text_id = Label(root, bg = '#ffc8dd', text = 'Quer namorar comigo?', fg = '#590d22', font = ('Montserrat', 24, 'bold'))
 text_id.pack()
-button_1 = tk.Button(root, text = 'Não', bg = '#ffb3c1', command = denied,
-                          relief = RIDGE, bd = 3, font = ('Montserrat', 8, 'bold'))
+button_1 = tk.Button(root, text = 'Não', bg = '#ffb3c1', command = denied, relief = RIDGE, bd = 3, font = ('Montserrat', 8, 'bold'))
 button_1.pack()
 root.bind('<Motion>', move_button_1)
-button_2 = tk.Button(root, text = 'Sim', bg = '#ffb3c1', relief = RIDGE,
-                     bd = 3, command = accepted, font = ('Montserrat', 14, 'bold'))
+button_2 = tk.Button(root, text = 'Sim', bg = '#ffb3c1', relief = RIDGE, bd = 3, command = accepted, font = ('Montserrat', 14, 'bold'))
 button_2.pack()
 
 root.mainloop()
